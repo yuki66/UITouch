abc52dcff39a54ec3f953009eee527cf3630f37f
yuki66 <nktiga6@gmail.com>
2015-04-17 18:56:40 +0900

    Initial Commit

---
+++ UITouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITouch
+//
+//  Created by YUKIKO HARADA on 2015/04/17.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITouch
+//
+//  Created by YUKIKO HARADA on 2015/04/17.
+//  Copyright (c) 2015年 yuki66. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
