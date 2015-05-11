048e3f9258d22525ae06270cc0de57c6a401edb3
yuki66 <nktiga6@gmail.com>
2015-05-11 19:06:14 +0900

    first commit

--- UITouch/ViewController.m
+++ UITouch/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+  
 @end
 
 @implementation ViewController
@@ -23,5 +23,24 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // タッチをどれか一つ取り出す。
+    // anyObjectはNSSetのメソッド。　適当に一つ返してくれる。
+    UITouch *t = [touches anyObject];
+    
+    // 画面上の座標を取得
+    CGPoint point = [t locationInView:self.view];
+    
+    // タッチされたところにView
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 15, 15)];
+    view.center = point;
+    view.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:view];
+    
+}
+
+
 
 @end
